dcc59fd6169f62907b6515f3faa9953f5fe8c126
kuure <kuure@msu.edu>
2026-08-26 15:04:25 -0400

    changes for fall 2026

--- demos/javascript-format-objects-from-api/scriptAPI.js
+++ demos/javascript-format-objects-from-api/scriptAPI.js
@@ -1,6 +1,6 @@
 // the url for any API that returns JSON data
 const url = "https://kuurola.com/api/json/pokedemo/";
-const apiKey = 'THIS IS MY API KEY'; 
+const apiKey = 'THIS IS MY API KEY';
 
 // the html container
 const container = document.querySelector("#container");
@@ -19,14 +19,14 @@ doTheWholeThing()
 async function doTheWholeThing() {
 
 	// first, get the remote data
-	const data = await getData(url,apiKey);
-	
+	const data = await getData(url, apiKey);
+
 	// when we have it, print to the console
 	console.log(data);
 
 	// then pass the data to a function that does *something* to it
 	const result = processData(data);
-	
+
 	// finally, print the processed data to the console 
 	// and insert the results into the container element
 	console.log(result);
@@ -41,8 +41,8 @@ function processData(dataArray) {
 
 	// use the .map() tool to chop up and format parts
 	// of the data 
-	const formattedData = dataArray.map( dataItem => {
-		return(`
+	const formattedData = dataArray.map(dataItem => {
+		return (`
 		<div class="item">
 			<div class="text">
 				<h1>${dataItem.name}</h1>
@@ -55,19 +55,20 @@ function processData(dataArray) {
 			</figure>
 
 		</div>
-		`)}).join("");
-		return(formattedData);
+		`)
+	}).join("");
+	return (formattedData);
 }
 
 
 
 
 // the function that fetches the remote data
-async function getData(url,key) {
+async function getData(url, key) {
 
 	// no API key needed
 	const response = await fetch(url);
-	
+
 	/* IF YOU NEED AN API KEY:
 	const response = await fetch(url, {
 		method: 'GET',
